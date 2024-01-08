d2ef7ebe36e2c9026deb7959ee7cb38df04999b4
Zhan Mingyang <zhan_my@outlook.com>
2024-01-08 11:19:57 +0800

    测试评论

---
+++ docs/.vitepress/theme/VssueLayout.vue
@@ -0,0 +1,32 @@
+<script lang="ts" setup>
+import { ref } from 'vue'
+import { VssueComponent } from 'vssue'
+import DefaultTheme from 'vitepress/theme'
+import GithubV4 from '@vssue/api-github-v4'
+
+const { Layout } = DefaultTheme
+// 在这里设置当前页面的 Issue 标题
+const title = ref<string>('Vssue Demo')
+
+// 注意，这里的 options 会覆盖上面通过 `Vue.use()` 设置的默认 options
+// 如果你不想改变它们，只需要传入 `title` prop 即可，不需要传入 `options` prop
+const options = ref<object>({
+  api: GithubV4,
+  owner: 'npm-run-start',
+  repo: 'https://github.com/npm-run-start/blog-Vssue',
+  clientId: 'eca57ce0d71548278034',
+  clientSecret: '0c234122dc5e5e99c69d3c4805047eed8475f33e', // 只有在使用某些平台时需要
+})
+</script>
+
+<template>
+  <Layout>
+    <template #doc-after>
+      <VssueComponent :title="title" :options="options" />
+    </template>
+  </Layout>
+</template>
+
+<style>
+@import 'vssue/dist/vssue.css';
+</style>
--- docs/.vitepress/theme/index.ts
+++ docs/.vitepress/theme/index.ts
@@ -1,17 +1,13 @@
 // https://vitepress.dev/guide/custom-theme
-import { h } from 'vue'
+import VssueLayout from './VssueLayout.vue'
 import type { Theme } from 'vitepress'
 import DefaultTheme from 'vitepress/theme'
 import './style.css'
 
 export default {
   extends: DefaultTheme,
-  Layout: () => {
-    return h(DefaultTheme.Layout, null, {
-      // https://vitepress.dev/guide/extending-default-theme#layout-slots
-    })
-  },
+  Layout: VssueLayout,
   enhanceApp({ app, router, siteData }) {
-    // ...
+    //
   },
 } satisfies Theme
