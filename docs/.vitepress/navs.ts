084690749aa9862fb16fc25eb46361a2bdbcc553
Zhan Mingyang <zhan_my@outlook.com>
2024-01-13 14:59:45 +0800

    git 操作

--- docs/.vitepress/navs.ts
+++ docs/.vitepress/navs.ts
@@ -35,6 +35,7 @@ export const navs = [
       { text: '帧动画', link: '/tools/keyframe' },
       { text: '背景音乐', link: '/tools/music' },
       { text: '微信授权登录', link: '/tools/wechat-auth' },
+      { text: 'Git 操作', link: '/tools/git' },
       { text: 'Linux 操作', link: '/tools/linux' },
       { text: 'ES6 +', link: '/tools/es6' },
     ],
